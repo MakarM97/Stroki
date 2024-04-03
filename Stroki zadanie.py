29bf62f0fa297826d3cb232f7cd87a5b7b1bced3
MakarM97 <165716716+MakarM97@users.noreply.github.com>
2024-04-04 03:07:31 +1000

    Add files via upload

--- Stroki zadanie.py
+++ Stroki zadanie.py
@@ -1,8 +1,8 @@
 name = "Makar Alexandrovich"
 print(name[0])
 print(name[-1])
-print(name[3:5])
+print(name[2:5])
 print(name[::-1])
-print(name, len)
+print(len(name))
 new_name = "Urban "
 print(new_name + name)
